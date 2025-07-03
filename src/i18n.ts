a6e47f911be87bde91d8d3632462df4e813f2a9f
Miguel Angel Pineda Vega <44409361+mapineda48@users.noreply.github.com>
2025-07-03 11:27:53 -0500

    Fix mobile layout (#4)
    * Improve mobile responsiveness
    
    * Improve mobile layout
    
    * Improve mobile header and hero
    
    * Improve welcome page mobile layout
    
    * fix mobile support
    
    * fix missing icons

--- src/i18n.ts
+++ src/i18n.ts
@@ -12,34 +12,45 @@ export const translations = {
           title: "Infrastructure Automation",
           description:
             "Automate your infrastructure management for scalability and reliability.",
+          iconPath: "M128,24C74.17,24,32,48.6,32,80v96c0,31.4,42.17,56,96,56s96-24.6,96-56V80C224,48.6,181.83,24,128,24Zm80,104c0,9.62-7.88,19.43-21.61,26.92C170.93,163.35,150.19,168,128,168s-42.93-4.65-58.39-13.08C55.88,147.43,48,137.62,48,128V111.36c17.06,15,46.23,24.64,80,24.64s62.94-9.68,80-24.64ZM69.61,53.08C85.07,44.65,105.81,40,128,40s42.93,4.65,58.39,13.08C200.12,60.57,208,70.38,208,80s-7.88,19.43-21.61,26.92C170.93,115.35,150.19,120,128,120s-42.93-4.65-58.39-13.08C55.88,99.43,48,89.62,48,80S55.88,60.57,69.61,53.08ZM186.39,202.92C170.93,211.35,150.19,216,128,216s-42.93-4.65-58.39-13.08C55.88,195.43,48,185.62,48,176V159.36c17.06,15,46.23,24.64,80,24.64s62.94-9.68,80-24.64V176C208,185.62,200.12,195.43,186.39,202.92Z"
         },
         {
           title: "Continuous Integration/Continuous Deployment (CI/CD)",
           description:
             "Implement CI/CD pipelines for faster and more reliable software releases.",
+          iconPath: "M69.12,94.15,28.5,128l40.62,33.85a8,8,0,1,1-10.24,12.29l-48-40a8,8,0,0,1,0-12.29l48-40a8,8,0,0,1,10.24,12.3Zm176,27.7-48-40a8,8,0,1,0-10.24,12.3L227.5,128l-40.62,33.85a8,8,0,1,0,10.24,12.29l48-40a8,8,0,0,0,0-12.29ZM162.73,32.48a8,8,0,0,0-10.25,4.79l-64,176a8,8,0,0,0,4.79,10.26A8.14,8.14,0,0,0,96,224a8,8,0,0,0,7.52-5.27l64-176A8,8,0,0,0,162.73,32.48Z"
         },
         {
           title: "Cloud Solutions",
           description:
             "Leverage cloud platforms to enhance your application's performance and availability.",
+          iconPath: "M160,40A88.09,88.09,0,0,0,81.29,88.67,64,64,0,1,0,72,216h88a88,88,0,0,0,0-176Zm0,160H72a48,48,0,0,1,0-96c1.1,0,2.2,0,3.29.11A88,88,0,0,0,72,128a8,8,0,0,0,16,0,72,72,0,1,1,72,72Z"
         },
       ],
       projectsTitle: "Recent Projects",
-      project1: {
-        title: "Automated Deployment Pipeline",
-        description:
-          "Streamlined deployment process for a large-scale application, reducing deployment time by 60%.",
-      },
-      project2: {
-        title: "Cloud Migration for E-commerce Platform",
-        description:
-          "Migrated an e-commerce platform to a cloud-based infrastructure, improving scalability and reducing costs.",
-      },
-      project3: {
-        title: "Enhanced Testing Automation",
-        description:
-          "Implemented automated testing frameworks, resulting in a 40% reduction in bugs and faster release cycles.",
-      },
+      projects: [
+        {
+          title: "Automated Deployment Pipeline",
+          description:
+            "Streamlined deployment process for a large-scale application, reducing deployment time by 60%.",
+          image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuAeTTfepcz5ZLh1DuCUMs7JjMjLSi3V1Mq8do_H1gS8SRGltvouEEg1-kb5knnNtmo4Bd9kpsJwR3StrP9kgTSLCd4uNK9x1g7QtTDtUS1VIEHUkmklU3oPWz0m9eA3fJnmg1hqwxPUkj3GawMDuUN78Lprp04YI_VldL6owssc6jPsKHNYra11mi_zVMPl030cCtb81SFSvJcq_DGY7MgfjjyOMJ8y_Hq-4zW7NkZHOayxss9VpFCYZU_BsGfvRjGaMmrGQn1-PgcC"
+        },
+        {
+          title: "Cloud Migration for E-commerce Platform",
+          description:
+            "Migrated an e-commerce platform to a cloud-based infrastructure, improving scalability and reducing costs.",
+          image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuDVXkEDGPi1tZZqx-qIYBUoCTc6b_rsBSs8-jz_XfMnkn0m2BJz7RvG0CT_NGDW45ksomvRRG2-YJqrlENLLeErWSNnCxWVVMV636R_YnJOOEPBcvWP6tvGJS59AOhcsAW8Me2Y27B--fN2Sirc_u-3giC41ii96S1tRTIEGaBmqjKjBXDKfP_lcG0Vp9VGywP2COBLUHbFix2ftAI-il56VArdSSZ5Pa94nEobObyx_wOQF9gkdgoI7p_v9J98eV15BcAU8ry_y8mB"
+        },
+        {
+          title: "Enhanced Testing Automation",
+          description:
+            "Implemented automated testing frameworks, resulting in a 40% reduction in bugs and faster release cycles.",
+          image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuC9lYcX2rd1g-5wcTgmGEtw9GcLWGkBbYw7QxqVtaMVrD7GqB-CR9hh4lMDDZctsv3N2AjRVwDDlA3a6BJ2fE5iT58Tj79YdDGF5BRyCcxc7TRn6uPcWlCLeu86y7owhNac16c3M5Jbvg7Hpk_dVU9HPR64WXOWhnNa8ZkiSOiv2F0CW-OrNGANpANZkceI7Ao314vnB1SF0dXvGCkucNxTCi8sHXuzIEyXk7AlhVaTwtDh7lSPcF3WgmHIh83usUlJB7DEVaF2VgWj"
+        }
+      ],
       footer: {
         privacy: "Privacy Policy",
         terms: "Terms of Service",
@@ -119,34 +130,47 @@ export const translations = {
           title: "Automatización de Infraestructura",
           description:
             "Automatiza la gestión de tu infraestructura para lograr escalabilidad y confiabilidad.",
+          iconPath: "M128,24C74.17,24,32,48.6,32,80v96c0,31.4,42.17,56,96,56s96-24.6,96-56V80C224,48.6,181.83,24,128,24Zm80,104c0,9.62-7.88,19.43-21.61,26.92C170.93,163.35,150.19,168,128,168s-42.93-4.65-58.39-13.08C55.88,147.43,48,137.62,48,128V111.36c17.06,15,46.23,24.64,80,24.64s62.94-9.68,80-24.64ZM69.61,53.08C85.07,44.65,105.81,40,128,40s42.93,4.65,58.39,13.08C200.12,60.57,208,70.38,208,80s-7.88,19.43-21.61,26.92C170.93,115.35,150.19,120,128,120s-42.93-4.65-58.39-13.08C55.88,99.43,48,89.62,48,80S55.88,60.57,69.61,53.08ZM186.39,202.92C170.93,211.35,150.19,216,128,216s-42.93-4.65-58.39-13.08C55.88,195.43,48,185.62,48,176V159.36c17.06,15,46.23,24.64,80,24.64s62.94-9.68,80-24.64V176C208,185.62,200.12,195.43,186.39,202.92Z"
+
         },
         {
           title: "Integración Continua/Entrega Continua (CI/CD)",
           description:
             "Implementa pipelines de CI/CD para lanzamientos de software más rápidos y confiables.",
+          iconPath: "M69.12,94.15,28.5,128l40.62,33.85a8,8,0,1,1-10.24,12.29l-48-40a8,8,0,0,1,0-12.29l48-40a8,8,0,0,1,10.24,12.3Zm176,27.7-48-40a8,8,0,1,0-10.24,12.3L227.5,128l-40.62,33.85a8,8,0,1,0,10.24,12.29l48-40a8,8,0,0,0,0-12.29ZM162.73,32.48a8,8,0,0,0-10.25,4.79l-64,176a8,8,0,0,0,4.79,10.26A8.14,8.14,0,0,0,96,224a8,8,0,0,0,7.52-5.27l64-176A8,8,0,0,0,162.73,32.48Z"
+
         },
         {
           title: "Soluciones en la Nube",
           description:
             "Aprovecha plataformas en la nube para mejorar el rendimiento y la disponibilidad de tus aplicaciones.",
+          iconPath: "M160,40A88.09,88.09,0,0,0,81.29,88.67,64,64,0,1,0,72,216h88a88,88,0,0,0,0-176Zm0,160H72a48,48,0,0,1,0-96c1.1,0,2.2,0,3.29.11A88,88,0,0,0,72,128a8,8,0,0,0,16,0,72,72,0,1,1,72,72Z"
         },
       ],
       projectsTitle: "Proyectos Recientes",
-      project1: {
-        title: "Pipeline de Despliegue Automatizado",
-        description:
-          "Proceso de despliegue optimizado para una aplicación a gran escala, reduciendo el tiempo de entrega en un 60%.",
-      },
-      project2: {
-        title: "Migración a la Nube para Plataforma E-commerce",
-        description:
-          "Migración de una plataforma de comercio electrónico a infraestructura en la nube, mejorando la escalabilidad y reduciendo costos.",
-      },
-      project3: {
-        title: "Mejora de Automatización de Pruebas",
-        description:
-          "Implementación de marcos de pruebas automatizadas, logrando una reducción del 40% de errores y ciclos de lanzamiento más rápidos.",
-      },
+      projects: [
+        {
+          title: "Pipeline de Despliegue Automatizado",
+          description:
+            "Proceso de despliegue optimizado para una aplicación a gran escala, reduciendo el tiempo de entrega en un 60%.",
+          image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuAeTTfepcz5ZLh1DuCUMs7JjMjLSi3V1Mq8do_H1gS8SRGltvouEEg1-kb5knnNtmo4Bd9kpsJwR3StrP9kgTSLCd4uNK9x1g7QtTDtUS1VIEHUkmklU3oPWz0m9eA3fJnmg1hqwxPUkj3GawMDuUN78Lprp04YI_VldL6owssc6jPsKHNYra11mi_zVMPl030cCtb81SFSvJcq_DGY7MgfjjyOMJ8y_Hq-4zW7NkZHOayxss9VpFCYZU_BsGfvRjGaMmrGQn1-PgcC"
+        },
+        {
+          title: "Migración a la Nube para Plataforma E-commerce",
+          description:
+            "Migración de una plataforma de comercio electrónico a infraestructura en la nube, mejorando la escalabilidad y reduciendo costos.",
+          image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuDVXkEDGPi1tZZqx-qIYBUoCTc6b_rsBSs8-jz_XfMnkn0m2BJz7RvG0CT_NGDW45ksomvRRG2-YJqrlENLLeErWSNnCxWVVMV636R_YnJOOEPBcvWP6tvGJS59AOhcsAW8Me2Y27B--fN2Sirc_u-3giC41ii96S1tRTIEGaBmqjKjBXDKfP_lcG0Vp9VGywP2COBLUHbFix2ftAI-il56VArdSSZ5Pa94nEobObyx_wOQF9gkdgoI7p_v9J98eV15BcAU8ry_y8mB"
+        },
+        {
+          title: "Mejora de Automatización de Pruebas",
+          description:
+            "Implementación de marcos de pruebas automatizadas, logrando una reducción del 40% de errores y ciclos de lanzamiento más rápidos.",
+          image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuC9lYcX2rd1g-5wcTgmGEtw9GcLWGkBbYw7QxqVtaMVrD7GqB-CR9hh4lMDDZctsv3N2AjRVwDDlA3a6BJ2fE5iT58Tj79YdDGF5BRyCcxc7TRn6uPcWlCLeu86y7owhNac16c3M5Jbvg7Hpk_dVU9HPR64WXOWhnNa8ZkiSOiv2F0CW-OrNGANpANZkceI7Ao314vnB1SF0dXvGCkucNxTCi8sHXuzIEyXk7AlhVaTwtDh7lSPcF3WgmHIh83usUlJB7DEVaF2VgWj"
+        }
+      ],
       footer: {
         privacy: "Política de Privacidad",
         terms: "Términos del Servicio",
